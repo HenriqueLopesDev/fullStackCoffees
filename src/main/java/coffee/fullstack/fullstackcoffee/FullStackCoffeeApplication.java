d4213f8dfdbff07d64737fde4d56f56a9b1cf58d
Henrique Lopes <henriquebonfanti2018@gmail.com>
2023-10-22 13:07:59 -0300

    chore: initial commit

---
+++ src/main/java/coffee/fullstack/fullstackcoffee/FullStackCoffeeApplication.java
@@ -0,0 +1,13 @@
+package coffee.fullstack.fullstackcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coffee/fullstack/fullstackcoffee/FullStackCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package coffee.fullstack.fullstackcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
